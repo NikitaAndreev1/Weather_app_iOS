0ae8e680bcc782649513051e8a2a8cd3dd19f542
Nikita Andreev <nikita.andreev.work@gmail.com>
2020-07-27 09:50:22 +0300

    Started creating a weather app

---
+++ Weather_app_iOS/AllCitiesCell.swift
@@ -0,0 +1,13 @@
+//
+//  AllCitiesCell.swift
+//  Weather_app_iOS
+//
+//  Created by Nicks on 27.07.2020.
+//  Copyright © 2020 Nicks. All rights reserved.
+//
+
+import UIKit
+
+class AllCitiesCell: UITableViewCell {
+   @IBOutlet weak var cityLabel: UILabel!
+}
---
+++ Weather_app_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_app_iOS
+//
+//  Created by Nicks on 27.07.2020.
+//  Copyright © 2020 Nicks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
